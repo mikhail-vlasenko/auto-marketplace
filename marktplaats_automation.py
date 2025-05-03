d612da1deef5d593c7cfd261284129c5c5e85c3e
Roman Knyazhitskiy <git@knyaz.tech>
2025-05-03 11:20:05 +0000

    fx

--- marktplaats_automation.py
+++ marktplaats_automation.py
@@ -704,6 +704,7 @@ class MarktplaatsAutomation:
                         if description_frame:
                             # We need to fill the tinymce body
 
+                            """
                             async def get_all_elements_recursive(locator):
                                 elements = await locator.locator("*").all()
                                 result = list(elements)
@@ -717,7 +718,8 @@ class MarktplaatsAutomation:
 
                             stuff = await get_all_elements_recursive(description_frame)
                             breakpoint()
-                            editor_body = description_frame.locator("body#tinymce")
+                            """
+                            editor_body = description_frame.locator("body")
 
                             if await editor_body.count() > 0:
                                 logging.info(
